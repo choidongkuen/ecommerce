a888a16baa992f5766e9ebedca0eccf6cca9bf21
choidongkuen <danaver12@daum.net>
2022-12-30 09:24:36 +0900

    [스프링][프로젝트][MailGun 기본 세팅]

---
+++ src/main/java/com/example/userapi/client/mailgun/SendMailForm.java
@@ -0,0 +1,19 @@
+package com.example.userapi.client.mailgun;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class SendMailForm {
+
+    public String form;
+    public String to;
+    public String subject;
+    public String text;
+}
